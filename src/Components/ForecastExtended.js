c47ff4733e4cf5ab4f1bac754e615279d1fad263
NazSau <cnazarenosaurral@gmail.com>
2018-09-24 18:09:36 -0300

    Cambios del 24/09

--- src/Components/ForecastExtended.js
+++ src/Components/ForecastExtended.js
@@ -1,17 +1,64 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
 import ForecastItem from './ForecastItem';
+import './styles.css';
 
+/*const days = [
+	"Lunes",
+	"Martes",
+	"Miercoles",
+	"Jueves",
+	"Vernes"
+]
+const data = {
+	temperature: 10,
+	humidity: 10,
+	weatherState: 'normal',
+	wind: "normal",
+}*/
+
+const ApiKey = "f6864fe6cb9c912b244a29b250e1aaea";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
 
 class ForecastExtended extends Component{
+
+	constructor(){
+		super();
+		this.state = {forecastData: null}
+	}
+	componentDidMount(){
+
+		const URL_forecast = `${url}?q=${this.props.city}&appid=${ApiKey}`;
+
+		fetch(URL_forecast).then(
+			data => (data.json())
+		).then(
+			wehater_data => {
+				console.log(wehater_data);
+			}
+		)
+	}
+
+	renderForecastItemDays(){
+		return "Datos del clima";
+		//return(days.map(day => <ForecastItem weekDay={day} hour={10} data={data}> </ForecastItem>));
+	}
+	renderProgress = () => {
+		return "Cargando pronostico extendido...";
+
+	}
 	render(){
 		const {city} = this.props;
+		const {forecastData} = this.state;
 		return (
 			<div>
 				<h2 className='Forecast-title'>
 					Pronostico extendido para {city}
 				</h2>
+				{	forecastData ?
+						this.renderForecastItemDays():
+						this.renderProgress()
+				}
 			</div>
 		);
 	}
--- src/Components/ForecastItem/index.js
+++ src/Components/ForecastItem/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import WeatherData from './../WeatherLocation/WeatherData';
 
-const ForecastItem = () => {
-	return(
-		<div>
-			Item
-		</div>
-	);
+
+class ForecastItem extends Component {
+	render(){
+		const {weekDay, hour, data} = this.props;
+		return(
+			<div>
+				<div>
+					{weekDay} 
+					Hora: {hour}hs
+				</div>
+				<WeatherData data={data}> </WeatherData>
+			</div>
+		);
+	}
 }
 
 export default ForecastItem;
