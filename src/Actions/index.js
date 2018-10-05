b38675b46f7ee558ef15c301b7dd3d28b003c767
NazSau <cnazarenosaurral@gmail.com>
2018-10-05 11:15:02 -0300

    Empezando con redux-thunk

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,3 +1,24 @@
 export const SET_CITY = 'SET_CITY';//ESTO ES POR CONVENSION DE CONSTANTES
 
 export const setCity = (payload) =>( {type: SET_CITY, payload}  )//por convencion es payload es lo mismo que value
+
+const ApiKey = "f6864fe6cb9c912b244a29b250e1aaea";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
+export const fetchForecast = payload =>{
+    return dispatch => {//esto lo da el middlware
+        //
+        const URL_forecast = `${url}?q=${city}&appid=${ApiKey}`;
+        
+        //accion inicial en el cual se establece que se hace una buscada, se activa en el estado un indicador de busqueda de datos
+		fetch(URL_forecast).then(
+			data => (data.json())
+		).then(
+			wehater_data => {
+				const forecastData = transformForecast(wehater_data);
+                console.log(forecastData);
+                // modificar el estado con el resultado de la primise, con el request(fetch)
+			}
+		);
+    };
+};
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,8 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import {city} from './../Reducers/City';
 const intialState = {
-    
+    city: 'Buenos Aires,ar'
 };
 
-export const store = createStore(city, intialState,
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(city, intialState, composeEnhacers(applyMiddleware(thunk)));
+ //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
